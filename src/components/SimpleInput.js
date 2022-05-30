b474120fc245e6cb844f8ac8ce0e7272ce9b2138
denisbao <denis.bao@cowmed.com.br>
2022-05-30 08:48:20 -0300

    202_Providing Validation Feedback

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,54 @@
-const SimpleInput = (props) => {
+import { useRef, useState } from 'react'
+
+const SimpleInput = props => {
+  // handling the input with state
+  const [enteredName, setEnteredName] = useState('')
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true)
+  
+  const nameInputChageHandler = event => {
+    setEnteredName(event.target.value)
+  }
+
+  // handling the input with refs
+  const nameInputRef = useRef()
+
+
+  const formSubmitHandler = event => {
+    event.preventDefault()
+
+    // validation based on the state
+    if(enteredName.trim() === '') {
+      setEnteredNameIsValid(false)
+      return
+    }
+    setEnteredNameIsValid(true)
+
+    console.log(`Input via state: ${enteredName}`)
+
+    const enteredValue = nameInputRef.current.value
+    console.log(`Input via refs: ${enteredValue}`)
+
+    setEnteredName('')
+  }
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className={enteredNameIsValid ? 'form-control' : 'form-control invalid'}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameInputChageHandler}
+        />
+        {!enteredNameIsValid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default SimpleInput;
+export default SimpleInput
