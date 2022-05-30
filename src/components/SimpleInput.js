036a12fe2d340467c57907e5cde05b784a3587d9
denisbao <denis.bao@cowmed.com.br>
2022-05-30 09:15:15 -0300

    205_Refactoring and Deriving States

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,47 +1,36 @@
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 
 const SimpleInput = props => {
+  
   // handling the input with state
   const [enteredName, setEnteredName] = useState('')
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
   const [enteredNameTouched, setEnteredNameTouched] = useState(false)
-  
+  // validation constants that are set based on the combine states above
+  const enteredNameIsValid = enteredName.trim() !== ''
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+
+
   const nameInputChageHandler = event => {
     setEnteredName(event.target.value)
   }
 
-  // handling the input with refs
-  const nameInputRef = useRef()
-
   const nameInputBlurHandler = event => {
     setEnteredNameTouched(true)
-    if(enteredName.trim() === '') {
-      setEnteredNameIsValid(false)
-      return
-    }
   }
-
 
   const formSubmitHandler = event => {
     event.preventDefault()
     setEnteredNameTouched(true)
 
-    // validation based on the state
-    if(enteredName.trim() === '') {
-      setEnteredNameIsValid(false)
+    if(!enteredNameIsValid) {
       return
     }
-    setEnteredNameIsValid(true)
 
     console.log(`Input via state: ${enteredName}`)
-
-    const enteredValue = nameInputRef.current.value
-    console.log(`Input via refs: ${enteredValue}`)
-
     setEnteredName('')
+    setEnteredNameTouched(false)
   }
 
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
   const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
 
   return (
@@ -49,7 +38,6 @@ const SimpleInput = props => {
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           value={enteredName}
